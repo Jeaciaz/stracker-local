13ceb11dba8ffaf86acf87fe2330cdcad4b1672b
Jeaciaz <sbeirakh@gmail.com>
2025-01-11 21:54:11 +0200

    temp: make it copy directly to clipboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ const App: Component = () => {
 
   return (
     <div class="p-4 min-h-svh flex flex-col gap-6">
-      <h1 class="text-4xl" onClick={() => prompt(JSON.stringify(localStorage))}>Track your ₪</h1>
+      <h1 class="text-4xl" onClick={() => navigator.clipboard.writeText(JSON.stringify(localStorage))}>Track your ₪</h1>
       <div role="tablist" class="tabs tabs-bordered">
         <button role="tab" classList={{ tab: true, 'tab-active': tab() === 'track' }} onPointerDown={() => setTab('track')}>Track</button>
         <button role="tab" classList={{ tab: true, 'tab-active': tab() === 'spendings' }} onPointerDown={() => setTab('spendings')}>Spendings</button>
