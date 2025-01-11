d52d57cfc5d29c1fd497d4aaac44f90e26bdb855
Jeaciaz <sbeirakh@gmail.com>
2025-01-11 22:02:40 +0200

    temp: add debug for parsing spendings

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -2,11 +2,15 @@ import {createEffect, createSignal} from 'solid-js'
 
 export const createLocalSignal = <T>(name: string, init: T) => {
   const localState = localStorage.getItem(name)
-  const [state, setState] = createSignal<T>(
-    localState ? JSON.parse(localState) : init,
-  )
-  createEffect(() => localStorage.setItem(name, JSON.stringify(state())))
-  return [state, setState] as const
+  try {
+    const [state, setState] = createSignal<T>(
+      localState ? JSON.parse(localState) : init,
+    )
+    createEffect(() => localStorage.setItem(name, JSON.stringify(state())))
+    return [state, setState] as const
+  } catch (e) {
+    alert(e instanceof Error ? e : JSON.stringify(e))
+  }
 }
 
 export const timestampPredicate = (
