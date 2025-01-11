e94d6a05a8898c3bf2bd04f8af23d0df467b1109
Jeaciaz <sbeirakh@gmail.com>
2025-01-11 21:48:30 +0200

    feat: add option to copy local storage using phone only

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,9 +6,10 @@ type Tab = 'track' | 'spendings';
 
 const App: Component = () => {
   const [tab, setTab] = createSignal<Tab>('track')
+
   return (
     <div class="p-4 min-h-svh flex flex-col gap-6">
-      <h1 class="text-4xl">Track your ₪</h1>
+      <h1 class="text-4xl" onClick={() => prompt(JSON.stringify(localStorage))}>Track your ₪</h1>
       <div role="tablist" class="tabs tabs-bordered">
         <button role="tab" classList={{ tab: true, 'tab-active': tab() === 'track' }} onPointerDown={() => setTab('track')}>Track</button>
         <button role="tab" classList={{ tab: true, 'tab-active': tab() === 'spendings' }} onPointerDown={() => setTab('spendings')}>Spendings</button>
